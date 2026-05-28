b4fda1f1a5893558e9a11688fc8bcbf0de6ce625
Sergey <shtervideo@gmail.com>
2026-05-28 06:59:25 +0000

    fix: cargo clippy v5

---
+++ src-tauri/target/debug/deps/thiserror-03cf3b17392fd723.d
@@ -0,0 +1,9 @@
+/workspaces/sd-wan-gaming-client/src-tauri/target/debug/deps/thiserror-03cf3b17392fd723.d: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/aserror.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/display.rs
+
+/workspaces/sd-wan-gaming-client/src-tauri/target/debug/deps/libthiserror-03cf3b17392fd723.rlib: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/aserror.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/display.rs
+
+/workspaces/sd-wan-gaming-client/src-tauri/target/debug/deps/libthiserror-03cf3b17392fd723.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/aserror.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/display.rs
+
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/lib.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/aserror.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/thiserror-1.0.69/src/display.rs:
